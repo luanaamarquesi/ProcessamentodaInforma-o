ec89b72b716a0657def66b5be3cfb07699dc3d72
luanaamarquesi <luana.marques@aluno.ufabc.edu.br>
2025-02-21 11:50:12 -0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,9 @@
+#Leia um número inteiro e escreva o seu sucessor e seu antecessor.
+
+num = int(input("Digite um número inteiro: "))
+antecessor = num - 1
+sucessor = num + 1
+
+
+print("O antecessor do número é: ", antecessor)
+print ("O sucessor do número é: ", sucessor)
---
+++ 5.py
@@ -0,0 +1,11 @@
+#Faça um algoritmo que receba 5 números inteiros e imprima a média aritmética dos 5.
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
+soma = num1 + num2 + num3 + num4 + num5
+med_aritm = soma / 5
+
+print("A média aritmética é: ", med_aritm)
